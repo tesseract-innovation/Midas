25a820988bb1f8f9b703026c2df08187a7206ea1
Dayvson Silva <userddssilva@gmail.com>
2025-08-19 23:24:54 -0400

    feat: implement layout ui for goals screen

--- screen/goals/src/main/kotlin/com/midasmoney/screen/goals/GoalsContent.kt
+++ screen/goals/src/main/kotlin/com/midasmoney/screen/goals/GoalsContent.kt
@@ -1,41 +1,86 @@
 package com.midasmoney.screen.goals
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.midasmoney.screen.goals.card.BalanceGoalStatusCard
+import com.midasmoney.screen.goals.card.CompletedGoalCard
+import com.midasmoney.screen.goals.card.GoalCard
+import com.midasmoney.screen.goals.card.MonthlyInsights
+import com.midasmoney.shared.model.mock.Database
+import com.midasmoney.shared.ui.core.component.MidasDarkPreview
+import com.midasmoney.shared.ui.core.component.MidasLightPreview
 
 @Composable
 fun GoalsContentImp() {
-    val goalsViewModel = GoalsViewModel()
-    GoalsContent(goalsViewModel.text.value.toString())
+    GoalsContent()
 }
 
 @Composable
-fun GoalsContent(text: String) {
+fun GoalsContent(
+    isDarkTheme: Boolean = isSystemInDarkTheme()
+) {
     Surface(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 30.dp, bottom = 100.dp)
     ) {
         Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
         ) {
-            Text(
-                text = text,
-                textAlign = TextAlign.Center
-            )
+            Column {
+                BalanceGoalStatusCard(
+                    balance = Database.balance,
+                )
+                TitleItem(
+                    textTitle = "Active Goals",
+                    textButton = "View All",
+                    actionButton = {}
+                )
+                Database.goalList.forEach {
+                    GoalCard(it)
+                }
+                TitleItem(
+                    textTitle = "Recently Completed",
+                    textButton = "View All",
+                    actionButton = {}
+                )
+                CompletedGoalCard()
+                CompletedGoalCard()
+                CompletedGoalCard()
+                MonthlyInsights(
+                    isDarkTheme = isDarkTheme
+                )
+            }
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GoalsContentImpPreview(){
-    val goalsViewModel = GoalsViewModel()
-    GoalsContent(goalsViewModel.text.value.toString())
+fun GoalsContentLightPreview() {
+    MidasLightPreview {
+        GoalsContent()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GoalsContentDarkPreview() {
+    MidasDarkPreview {
+        GoalsContent(true)
+    }
 }
---
+++ screen/goals/src/main/kotlin/com/midasmoney/screen/goals/card/MonthlyInsightsCard.kt
@@ -0,0 +1,140 @@
+package com.midasmoney.screen.goals.card
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Analytics
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.midasmoney.shared.resource.R.string.monthly_insights
+import com.midasmoney.shared.resource.R.string.monthly_save
+import com.midasmoney.shared.resource.R.string.monthly_target
+import com.midasmoney.shared.resource.R.string.monthly_track
+import com.midasmoney.shared.ui.core.color.MidasColors
+import com.midasmoney.shared.ui.core.component.MidasDarkPreview
+import com.midasmoney.shared.ui.core.component.MidasGradientCard
+import com.midasmoney.shared.ui.core.component.MidasLightPreview
+
+@Composable
+fun MonthlyInsights(
+    isDarkTheme: Boolean = isSystemInDarkTheme()
+) {
+    MidasGradientCard(
+        primaryColor = MidasColors.Orange.primary,
+        secondaryColor = MidasColors.Pink.primary,
+        height = 163.dp,
+        isDarkTheme = isDarkTheme
+    ) {
+        Column {
+            Row(
+                modifier = Modifier.padding(start = 19.dp, top = 24.dp)
+            ) {
+                Column {
+                    Icon(
+                        imageVector = Icons.Outlined.Analytics,
+                        contentDescription = Icons.Outlined.Analytics.name,
+                        tint = MaterialTheme.colorScheme.onPrimaryContainer,
+                        modifier = Modifier.size(44.dp),
+                    )
+                }
+                Column(
+                    modifier = Modifier
+                        .padding(start = 12.dp)
+                ) {
+                    Row {
+                        Text(
+                            text = stringResource(monthly_insights),
+                            fontSize = 16.sp,
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                    Row {
+                        Text(
+                            text = stringResource(monthly_track),
+                            fontWeight = FontWeight.W300,
+                            fontSize = 12.sp,
+                            color = Color.White,
+                        )
+                    }
+                }
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 24.dp, bottom = 28.dp, top = 18.dp)
+            ) {
+                Column {
+                    Row {
+                        Text(
+                            text = stringResource(monthly_save),
+                            fontWeight = FontWeight.W300,
+                            fontSize = 13.sp,
+                            color = Color.White,
+                        )
+                    }
+                    Row {
+                        Text(
+                            text = "$850",
+                            fontSize = 21.sp,
+                            color = Color.White,
+                            fontWeight = FontWeight.W400
+                        )
+                    }
+                }
+                Column(
+                    modifier = Modifier
+                        .padding(start = 46.dp)
+                ) {
+                    Row {
+                        Text(
+                            text = stringResource(monthly_target),
+                            fontWeight = FontWeight.W300,
+                            fontSize = 13.sp,
+                            color = Color.White,
+                        )
+                    }
+                    Row {
+                        Text(
+                            text = "$850",
+                            fontSize = 21.sp,
+                            color = Color.White,
+                            fontWeight = FontWeight.W400
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = android.graphics.Color.WHITE.toLong())
+@Composable
+fun MonthlyInsightsLightPreview() {
+    MidasLightPreview {
+        MonthlyInsights()
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = android.graphics.Color.BLACK.toLong())
+@Composable
+fun MonthlyInsightsDarkPreview() {
+    MidasDarkPreview {
+        MonthlyInsights(
+            isDarkTheme = true
+        )
+    }
+}
